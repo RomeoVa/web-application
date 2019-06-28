2e027e2e23e57769eef4460b36d7b018e26e8799
JesusC10 <50536470+JesusC10@users.noreply.github.com>
2019-06-27 21:08:04 -0500

    lb4(models, ds, repository, controller)

---
+++ lb4/facturas-api/src/models/cliente.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {}})
+export class Cliente extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  apellido: string;
+
+  @property({
+    type: 'string',
+    id: true,
+    required: true,
+  })
+  rfc: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  contrasena: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  actaConstitutiva: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  estadoDeCuenta: string;
+
+
+  constructor(data?: Partial<Cliente>) {
+    super(data);
+  }
+}
+
+export interface ClienteRelations {
+  // describe navigational properties here
+}
+
+export type ClienteWithRelations = Cliente & ClienteRelations;
