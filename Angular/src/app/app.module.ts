e9fcc57cfaa3e9dd94d8de633b5c63e7499b62f2
romeova <romeova.7243@gmail.com>
2019-07-01 00:51:31 -0500

    Updated factura component

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -54,7 +54,7 @@ const routes: Routes = [
         component: PerfilComponent
       },
       {
-        path: 'factura',
+        path: 'factura/:id',
         component: FacturaComponent
       },
       {
