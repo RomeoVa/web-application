b5dd897b3407bc3eb7c75cbc31685dcf5924c3ce
JesusC10 <50536470+JesusC10@users.noreply.github.com>
2019-07-04 19:17:58 -0500

    .

--- lb4/facturas-api/src/services/validator.ts
+++ lb4/facturas-api/src/services/validator.ts
@@ -4,14 +4,14 @@ import {HttpErrors} from '@loopback/rest';
 
 export function validateCredentials(credentials: CredentialsEmpresa) {
   // Validate Email
-  if (!isemail.validate(credentials.correo)) {
+  /*if (!isemail.validate(credentials.correo)) {
     throw new HttpErrors.UnprocessableEntity('invalid email');
-  }
+  }*/
 
   // Validate Email
-  /*if (!credentials.rfc.length == 12 ) {
+  if (!credentials.rfc.length == 12 ) {
     throw new HttpErrors.UnprocessableEntity('invalid rfc');
-  }*/
+  }
 
   // Validate Password Length
   if (credentials.contrasena.length < 8) {
